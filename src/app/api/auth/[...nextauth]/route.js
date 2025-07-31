124698c66c1c3aba07527809af7c76f4b80e7695
Cameron Low <low.cameron3@gmail.com>
2025-07-31 13:43:26 -0400

    Updated path

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,6 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
-import excuteQuery from "../../../../../lib/db";
+import excuteQuery from "@/lib/db";
 
 export const authOptions = {
     providers: [
