46af5c08220402681568f82d314559c235bc2327
Cameron Low <low.cameron3@gmail.com>
2025-07-31 13:21:24 -0400

    Change paths

--- src/app/api/assets/update/route.js
+++ src/app/api/assets/update/route.js
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 import excuteQuery from "@/lib/db";
-import { getAssetById } from "@/lib/assetHelper"; // adjust path as needed
+import { getAssetById } from "../../../../../lib/assetHelper";
 
 export async function POST(request) {
     try {
